2c7028251e8c3cdfaf05148256a68e40bab5d4a8
chltjgus <chltjgus0211@gmail.com>
2024-01-19 12:30:29 +0900

    test.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,9 +2,19 @@ from django.urls import path
 
 from . import views
 
+# html파일에서 href로 끌어다 쓸 때 어디에 위치해 있는지 알려주기 위해 app_name을 지정
+# html 파일에선 href="{% url 'polls:detail' question.id %}" 이런식으로 표기
+app_name = "polls"
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results,name="results"),
+    # path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
+
+    # path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+
+    # path("<int:question_id>/results/", views.results,name="results"),
+    path("<int:pk>/results/", views.ResultView.as_view(), name="detail"),
+
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import context
+from django.urls import reverse
+from django.views import generic
+
 # Create your views here.
 
-from .models import Question
+from .models import Question, Choice
+from django.utils import timezone
+
+
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "detail.html"
+
+    def get_queryset(self):
+        return
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "results.html"
 
 
 def index(request):
@@ -13,13 +38,44 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    # 방법 1
+    # try:
+    #     question=Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question이 없어요")
+    # response = "You're looking at the results of question %s."
+
+    # 방법 2( 더 간단한 방법 )
+    question = get_object_or_404(Question, pk=question_id)
+
+
+    return render(request, "detail.html", {"question": question})
+  #  return HttpResponse(f"You're looking at question {question_id}")
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
